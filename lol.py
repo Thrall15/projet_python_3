9ed402cf2d064a4d8c89d54749c22b7a592317da
Thrall15 <157879489+Thrall15@users.noreply.github.com>
2025-04-03 11:47:03 +0300

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,10 +1,1 @@
-import pyautogui
-import time
-
-texte = "Salut toi ! tu vas bien ? 💕 "
-
-time.sleep(30)
-for t in range(0,3) :
-    for i in range(0,len(texte)) :
-        pyautogui.press(texte[i])
-    time.sleep(10)
+# tafiditra am tss antony mdr
